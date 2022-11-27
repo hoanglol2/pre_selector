c8eaf516d8cad1563d8ec9fc92e8ad68eff0175a
Beers <duyhoangpt98@gmail.com>
2022-11-27 22:31:32 +0700

    remove old file

--- app/src/main/java/com/example/picker_selector_app/BaseFragment.kt
+++
@@ -1,9 +0,0 @@
-package com.example.picker_selector_app
-
-import android.view.View
-import androidx.fragment.app.Fragment
-
-open class BaseFragment: Fragment(), View.OnClickListener {
-
-    override fun onClick(v: View?) = Unit
-}
